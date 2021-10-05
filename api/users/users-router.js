470dfa4ef8f7bb367f0c644ad5cefa150d1c23a5
stkirk <shaanekirk@gmail.com>
2021-10-05 16:25:07 -0400

    post and put users working with middleware validation

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -22,15 +22,25 @@ router.get("/:id", validateUserId, (req, res) => {
   res.status(200).json(req.user);
 });
 
-router.post("/", validateUser, (req, res) => {
+router.post("/", validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  Users.insert(req.body)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
-router.put("/:id", validateUserId, validateUser, (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id, req.body)
+    .then((updatedUser) => {
+      res.status(200).json(updatedUser);
+    })
+    .catch(next);
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
